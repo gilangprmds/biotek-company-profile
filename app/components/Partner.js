ccf3b492c7dbcf5856016658d8ae85b824eb5489
gilangprmds <gilangpramudias@gmail.com>
2025-08-03 18:57:24 +0700

    fix image partner

--- app/components/Partner.js
+++ app/components/Partner.js
@@ -33,7 +33,7 @@ const clients = [
   },
   {
     title: "RS EMC Tangerang",
-    image: "/img/clients/RS EMC Tangerang.png",
+    image: "/img/clients/RS EMC TANGERANG.png",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
