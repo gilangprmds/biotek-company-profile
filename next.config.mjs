d447d845d00027a3f9e1d7b398fc1e3480e881ad
adam <adamikhsan89@gmail.com>
2025-08-09 02:59:56 +0700

    fix domain image

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost', 'https://biotek.co.id/api'],
+    domains: ['localhost', 'biotek.co.id'],
   },
 };
 
